08b80d13ab188fe5079e3e532a7d99475d20784e
gthomas <gthomas@geologging.com>
2019-02-12 13:19:20 +0000

    formatting

--- src/conditional-if/conditional-if.ts
+++ src/conditional-if/conditional-if.ts
@@ -4,14 +4,14 @@ import { bindable, useView } from "aurelia-templating";
 @useView(PLATFORM.moduleName("./conditional-if.html"))
 export class ConditionalIf {
 
-    @bindable public expression: any;
+  @bindable public expression: any;
 
-    public constructor(expression?: any) {
-        this.expression = expression || false;
-    }
+  public constructor(expression?: any) {
+    this.expression = expression || false;
+  }
 
-    public evaluate(): boolean {
-        return !! eval(this.expression);
-    }
+  public evaluate(): boolean {
+    return !! eval(this.expression);
+  }
 
 }
